5dbe368136345d2734b9d4c5a2df6a7a9439048c
rahulbhati20 <rahulbhati53514@gmail.com>
2024-08-02 08:58:31 +0530

    final touch done

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
+// =============================== Navbar Icon ============================================
+
 let menuIcon = document.querySelector('#menu');
 let navbar = document.querySelector('.nav');
 
-menuIcon.onclick = () =>{
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-xmark');
     navbar.classList.toggle('active');
 };
 
-
-let section = document.querySelectorAll('section');
+// ============================= Scroll Section ======================================
+let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    section.forEach(sec => {
+    sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
-            navlinks.forEach.apply(links =>{
-                links.classList.remove('active');
-                document.querySelector('header nav a[href* ' + id + ']').classList.add('active');
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector(`header nav a[href*=${id}]`).classList.add('active');
             });
-        };
-    } );
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky',window.scrollY > 100);
+        }
+    });
 
-}
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+// ScrollReveal initialization
+ScrollReveal({ 
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.homeimg, .skill-container, .project-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+// Typed.js initialization
+const typed = new Typed('.multiple-text', {
+    strings: ['frontend developer', 'web designer', 'python developer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
